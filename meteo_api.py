0b67467e3f2d9ec3756479d3c1f9ab2990280c39
zChrees <chriperriera2004@gmail.com>
2026-09-08 16:22:40 +0200

    christian completato

---
+++ meteo_api.py
@@ -0,0 +1,64 @@
+# meteo_api.py — STUDENTE 1 — branch: feature/api
+# Funzioni: cerca_coordinate(nome_citta)
+#           scarica_dati_storici(latitudine, longitudine, data_inizio, data_fine)
+import requests
+
+URL_GEOCODING = "https://geocoding-api.open-meteo.com/v1/search"
+URL_ARCHIVIO = "https://archive-api.open-meteo.com/v1/archive"
+
+def cerca_coordinate(nome_citta):
+    """
+    Chiede all'API di geocoding le coordinate di una citta'.
+    Ritorna una coppia (latitudine, longitudine), oppure None
+    se la citta' non viene trovata.
+    """
+    parametri = {
+        "nome" : nome_citta, 
+        "count" : 1,
+        "language" : "it",
+        "format" : "json"
+    }
+    risposta = requests.get(URL_GEOCODING, params=parametri, timeout=10)
+    dati = risposta.json()
+
+    if "results" in dati:
+        primo_risultato = dati["results"][0]
+        latitudine = primo_risultato["latitude"]
+        longitudine = primo_risultato["longitude"]
+        return latitudine, longitudine
+    else:
+        print("Citta' non trovata")
+        return None
+    pass
+
+
+def scarica_dati_storici(latitudine, longitudine, data_inizio, data_fine):
+    """
+    Chiama l'API Open-Meteo Archive e ritorna il dizionario JSON
+    con le temperature storiche nell'intervallo richiesto.
+    Ritorna None se c'e' un errore.
+    """
+    parametri = {
+        "latitude" : latitudine, 
+        "longitude" : longitudine,
+        "start_date" : data_inizio,
+        "end_date" : data_fine,
+        "daily" : "temperature_2m_max,temperature_2m_min",
+        "timezone" : "Europe/Rome"
+    }
+
+    try:
+        risposta = requests.get(URL_ARCHIVIO, params=parametri, timeout=10)
+        if risposta.status_code == 200:
+            return risposta.json()
+        else:
+            print("Errore: ")
+            return None
+
+    except requests.exceptions.Timeout:
+        print("Errore: ")
+        return None
+
+    except requests.exceptions.ConnectionError:
+        print("Errore: ")
+        return None
