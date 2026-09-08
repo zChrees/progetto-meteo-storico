557845ea0f23b476d9ed4e5918967d7733fed834
zChrees <chriperriera2004@gmail.com>
2026-09-08 18:35:18 +0200

    bug sistemati in feature/display

--- meteo_display.py
+++ meteo_display.py
@@ -1,19 +1,18 @@
 def stampa_previsioni (previsioni, citta):
 
-print(f "PREVISIONI STORICHE - {citta}")
-print("=" * 34)
-print("GIORNO      | MAX (°C) | MIN (°C)")
-print("-" * 34)
+    print(f"PREVISIONI STORICHE - {citta}")
+    print("=" * 34)
+    print("GIORNO      | MAX (°C) | MIN (°C)")
+    print("-" * 34)
 
-for giorno in previsioni:
-    riga= f"{giorno['data']:<12} | {giorno['temp_max']:>8.1f} | {giorno['temp_min']:>8.1f}"
-    print(riga)
+    for giorno in previsioni:
+        riga= f"{giorno['data']:<12} | {giorno['temp_max']:>8.1f} | {giorno['temp_min']:>8.1f}"
+        print(riga)
 
 
 def stampa_statistiche(statistiche) :
-    print()
-    print("STATISTICHE")
-    PRINT("=" * 34)
+    print("\nSTATISTICHE")
+    print("=" * 34)
     print(f"Media temperatura massima: {statistiche['media_max']:.1f}°C")
 
     caldo = statistiche["giorno_piu_caldo"]
